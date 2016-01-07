db4d070bcc33bab161fc04971f71059e657b0d04
StarCYing <sying@doc.gov>
2016-01-07 09:47:01 -0500

    it works!

--- tag.py
+++ tag.py
@@ -1,80 +1,47 @@
 import re, json, pprint
-from time import time
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.linear_model import SGDClassifier
-from sklearn.grid_search import GridSearchCV
-from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import Normalizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.cluster import KMeans
+from sklearn.pipeline import make_pipeline
 from tqdm import *
-from sklearn.datasets import fetch_20newsgroups
 
 pp = pprint.PrettyPrinter(indent=2)
 
-with open('./noaa.json', 'rb') as f:
-    noaa = json.load(f)
-
-# categories = [
-#     'alt.atheism',
-#     'talk.religion.misc',
-# ]
-# # Uncomment the following to do the analysis on all the categories
-# #categories = None
-
-# print("Loading 20 newsgroups dataset for categories:")
-# print(categories)
-
-# data = fetch_20newsgroups(subset='train', categories=categories)
-# print("%d documents" % len(data.filenames))
-# print("%d categories" % len(data.target_names))
-# print()
-
-# print data
-
-pipeline = Pipeline([
-    ('vect', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-    # ('clf', SGDClassifier()),
-])
-
-parameters = {
-    'vect__max_df': (0.5, 0.75, 1.0),
-    #'vect__max_features': (None, 5000, 10000, 50000),
-    'vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
-    #'tfidf__use_idf': (True, False),
-    #'tfidf__norm': ('l1', 'l2'),
-    'clf__alpha': (0.00001, 0.000001),
-    'clf__penalty': ('l2', 'elasticnet'),
-    #'clf__n_iter': (10, 50, 80),
-}
+def show_keywords(datajson, centroids, terms):
+    n_clusters = centroids.shape[0]
+    keyword_clusters = []
+    for i in range(n_clusters):
+        keyword_clusters.append([ terms[k] for k in centroids.argsort()[:,::-1][i, :20] ])
+        for ind in centroids.argsort()[:,::-1][i, :20]:
+            print ' %s' % terms[ind]
+        print
+    return keyword_clusters
 
 if __name__ == "__main__":
-    data = []
+    print 'Preprocessing'
+    with open('./noaa.json', 'rb') as f:
+        noaa = json.load(f)
+    X_pre = []
     for entry in tqdm(noaa):
-        # data.extend(re.split('\s', entry[u'description']))
-        # for chain in entry[u'keyword']:
-        #     data.extend(re.split('>', chain))
-        data.append(entry[u'description'])
-        data.extend(entry[u'keyword'])
+        X_pre.append(entry[u'description'])
+        X_pre.extend(entry[u'keyword'])
+    stopwords = ['department', 'of', 'commerce', 'doc', 'noaa', 'national', 'data', 'and', 'the', 'for', 'centers', 'united', 'states']
+    tfidf = TfidfVectorizer(ngram_range=(1,2), stop_words=stopwords)
+    X_tfidf = tfidf.fit_transform(X_pre)
+    print 'Output feature name list'
+    with open('feature_names.json', 'wb') as f:
+        json.dump(tfidf.get_feature_names(), f)
+    print 'Dimensionality Reduction'
+    lsa = TruncatedSVD(n_components=5)
+    X_lsa = lsa.fit_transform(X_tfidf)
+    norm = Normalizer()
+    X_norm = norm.fit_transform(X_lsa)
+    print 'Clustering'
+    kmeans = KMeans(n_clusters=30, max_iter=100)
+    kmeans.fit(X_norm)
+    centroids = lsa.inverse_transform(kmeans.cluster_centers_)
+    print 'Get Keywords'
+    with open('examine_keys.json', 'wb') as f:
+        json.dump(show_keywords(noaa, centroids, tfidf.get_feature_names()), f)
 
-    X = pipeline.fit_transform(data)
-    print X.shape
-    # multiprocessing requires the fork to happen in a __main__ protected
-    # block
-
-    # find the best parameters for both the feature extraction and the
-    # classifier
-    # grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1)
-
-    # print("Performing grid search...")
-    # print("pipeline:", [name for name, _ in pipeline.steps])
-    # print("parameters:")
-    # pp.pprint(parameters)
-    # t0 = time()
-    # grid_search.fit(data.data, data.target)
-    # print("done in %0.3fs" % (time() - t0))
-    # print()
-
-    # print("Best score: %0.3f" % grid_search.best_score_)
-    # print("Best parameters set:")
-    # best_parameters = grid_search.best_estimator_.get_params()
-    # for param_name in sorted(parameters.keys()):
-    #     print("\t%s: %r" % (param_name, best_parameters[param_name]))
