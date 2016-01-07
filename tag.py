2922cf5aa0c437f75748f5e72cde107fb6f40a67
StarCYing <sying@doc.gov>
2016-01-07 16:27:34 -0500

    pipeline

--- tag.py
+++ tag.py
@@ -8,18 +8,15 @@ from tqdm import *
 
 pp = pprint.PrettyPrinter(indent=2)
 
-def show_keywords(datajson, centroids, terms):
+def show_keywords(centroids, terms):
     n_clusters = centroids.shape[0]
     keyword_clusters = []
     for i in range(n_clusters):
         keyword_clusters.append([ terms[k] for k in centroids.argsort()[:,::-1][i, :20] ])
-        for ind in centroids.argsort()[:,::-1][i, :20]:
-            print ' %s' % terms[ind]
-        print
     return keyword_clusters
 
 if __name__ == "__main__":
-    print 'Preprocessing'
+    # Open file
     with open('./noaa.json', 'rb') as f:
         noaa = json.load(f)
     X_pre = []
@@ -27,21 +24,27 @@ if __name__ == "__main__":
         X_pre.append(entry[u'description'])
         X_pre.extend(entry[u'keyword'])
     stopwords = ['department', 'of', 'commerce', 'doc', 'noaa', 'national', 'data', 'and', 'the', 'for', 'centers', 'united', 'states']
-    tfidf = TfidfVectorizer(ngram_range=(1,2), stop_words=stopwords)
-    X_tfidf = tfidf.fit_transform(X_pre)
-    print 'Output feature name list'
+    tfidf = TfidfVectorizer(ngram_range=(1,2), stop_words=stopwords).fit(X_pre)
+    # Dump for once over
     with open('feature_names.json', 'wb') as f:
         json.dump(tfidf.get_feature_names(), f)
-    print 'Dimensionality Reduction'
     lsa = TruncatedSVD(n_components=5)
-    X_lsa = lsa.fit_transform(X_tfidf)
     norm = Normalizer()
-    X_norm = norm.fit_transform(X_lsa)
-    print 'Clustering'
     kmeans = KMeans(n_clusters=30, max_iter=100)
-    kmeans.fit(X_norm)
+    pipeline = make_pipeline(tfidf, lsa, norm, kmeans)
+    pipeline.fit_transform(X_pre)
     centroids = lsa.inverse_transform(kmeans.cluster_centers_)
-    print 'Get Keywords'
     with open('examine_keys.json', 'wb') as f:
-        json.dump(show_keywords(noaa, centroids, tfidf.get_feature_names()), f)
+        json.dump(show_keywords(centroids, tfidf.get_feature_names()), f)
+    X_test = [ noaa[0][u'description'] ]
+    X_test.extend(noaa[0][u'keyword'])
+    print len(X_test)
+    cluster_test = pipeline.predict(X_test)
+    pp.pprint(cluster_test)
+    print cluster_test.shape
+    # Predict
+    # Build set
+    # X_hat = { entry[u'title']: }
+
+
 
