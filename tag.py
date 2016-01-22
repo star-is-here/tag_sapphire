6503b100035319ac9995556d4f9d642ca2c98363
StarCYing <sying@doc.gov>
2016-01-22 10:23:21 -0500

    refinements and new method
    Added usability/readability changes to the code and added initial DPGMM
    topic generation

--- tag.py
+++ tag.py
@@ -1,8 +1,9 @@
 import re, json, pprint, time
 from sklearn.preprocessing import Normalizer
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, ENGLISH_STOP_WORDS
 from sklearn.decomposition import TruncatedSVD, LatentDirichletAllocation
 from sklearn.cluster import KMeans
+from sklearn.mixture import DPGMM
 from sklearn.pipeline import make_pipeline
 from tqdm import *
 
@@ -12,21 +13,21 @@ pp = pprint.PrettyPrinter(indent=2)
 def print_top_words(model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
         print("Topic #%d:" % topic_idx)
-        print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
+        print(" ".join([ feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1] ]))
     print()
 
 def save_top_words(model, feature_names, n_top_words):
     wordme = {}
     for topic_idx, topic in tqdm(enumerate(model.components_)):
-        wordme["Topic #%d:" % topic_idx] = [ feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1] ]
+        wordme["Topic #%d" % topic_idx] = [ feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1] ]
     return wordme
 
-def show_keywords(centroids, terms):
+def show_keywords(centroids, terms, n_top_words):
     n_clusters = centroids.shape[0]
-    keyword_clusters = []
-    for i in range(n_clusters):
-        keyword_clusters.append([ terms[k] for k in centroids.argsort()[:,::-1][i, :20] ])
+    keyword_clusters = {}
+    for i in tqdm(range(n_clusters)):
+        # keyword_clusters['Topic #%d' % i] = [ terms[k] for k in centroids.argsort()[:,::-1][i, :n_top_words] ]
+        keyword_clusters['Topic #%d' % i] = [ terms[k] for k in centroids.argsort()[:-n_top_words - 1:-1]]
     return keyword_clusters
 
 # Collapse documents per dataset to get better idf
@@ -41,17 +42,20 @@ if __name__ == "__main__":
     # Setting Options
     #####################################################################################################################
     # Number of features to keep after weighting
-    prop_feat = 20000
+    prop_feat = 1000
     # Number of components to keep after dimension reduction
-    prop_comp = 200
+    prop_comp = 100
     # Number of clusters to generate
-    prop_clust = 200 
+    prop_clust = 60
+    # Words to keep
+    prop_words = 30
     # Stopwords to exclude
-    stopwords = ['department', 'of', 'commerce', 'doc', 'noaa', 'national', 'data', 'and', 'the', 'for', 'centers', 'united', 'states']
+    add_stopwords = ['department', 'of', 'commerce', 'doc', 'noaa', 'national', 'data', 'and', 'the', 'for', 'centers', 'united', 'states']
+    stopwords = ENGLISH_STOP_WORDS.union(add_stopwords)
     print '#####################################################################################################################'
     print 'Options selected:'
     print '#####################################################################################################################'
-    print '# of features retained: %3d, # of components: %3d, # of clusters: %3d'%(int(prop_feat), int(prop_comp), int(prop_clust))
+    print '# of features retained: %s, # of components: %s, # of clusters: %s, # of tags: %s'%(prop_feat, prop_comp, prop_clust, prop_words)
     print '#####################################################################################################################'
     print 'Stopwords used:'
     print '#####################################################################################################################'
@@ -66,8 +70,6 @@ if __name__ == "__main__":
         noaa = json.load(f)
     X_pre = []
     for entry in tqdm(noaa):
-        # X_pre.append(entry[u'description'])
-        # X_pre.extend(entry[u'keyword'])
         X_pre.append(create_doc(entry))
     #####################################################################################################################
     # Processing ngrams to weight
@@ -76,11 +78,12 @@ if __name__ == "__main__":
     print 'Weighting ngrams'
     print '#####################################################################################################################'
     t0 = time.time()
+    print 'Time started: %s'%t0
     vect = TfidfVectorizer(ngram_range=(1,3), stop_words=stopwords, max_features=prop_feat)
     # vect = CountVectorizer(ngram_range=(1,1), stop_words=stopwords, max_features=prop_feat)
     X_vect = vect.fit_transform(X_pre)
     print 'Time elapsed: %s'%(time.time()-t0)
-    pp.pprint(X_vect.shape)
+    print 'n_samples: %s, n_features: %s'%X_vect.shape
     with open('feature_list.json', 'wb') as f:
         json.dump(vect.get_feature_names(), f)
     #####################################################################################################################
@@ -90,43 +93,58 @@ if __name__ == "__main__":
     print 'Dimensionality reduction'
     print '#####################################################################################################################'
     t0 = time.time()
-    # dimred = TruncatedSVD(n_components=prop_comp, n_jobs=-1)
-    dimred = LatentDirichletAllocation(n_topics=prop_comp, max_iter=5, n_jobs=1)
-    dimred.fit(X_vect)
-    print 'Time elapsed: %s'%(time.time()-t0)
-    # pp.pprint(dimred.components_)
-    # print_top_words(dimred, vect.get_feature_names(), 30)
-    with open('dimensions_kept.json', 'wb') as f:
-        json.dump(save_top_words(dimred, vect.get_feature_names(), 30), f)
+    print 'Time started: %s'%t0
+    dim = TruncatedSVD(n_components=prop_comp)
+    X_dim = dim.fit_transform(X_vect)
+    print 'LSA Time elapsed: %s'%(time.time()-t0)
+
+    # pp.pprint(dim.components_)
     #####################################################################################################################
     # L2 distance normalizer for K-means clustering
     #####################################################################################################################
-    # print '#####################################################################################################################'
-    # print 'Nomalizing'
-    # print '#####################################################################################################################'
-    # norm = Normalizer()
+    print '#####################################################################################################################'
+    print 'Nomalizing'
+    print '#####################################################################################################################'
+    t0 = time.time()
+    print 'Time started: %s'%t0
+    norm = Normalizer()
+    X_norm = norm.fit_transform(X_dim)
+    print 'Time elapsed: %s'%(time.time()-t0)
     #####################################################################################################################
-    # K-means clustering on reduced dimensions
+    # Topic Generation
     #####################################################################################################################
     # print '#####################################################################################################################'
-    # print 'Clustering'
+    # print 'Topic Generation: KMeans'
     # print '#####################################################################################################################'
-    # kmeans = KMeans(n_clusters=prop_clust, max_iter=100)
-    # # pipeline_lsa = make_pipeline(tfidf, lsa, norm, kmeans)
-    # pipeline_lda = make_pipeline(cntvect, lda, norm, kmeans)
-    # pipeline_lda.fit_transform(X_pre)
-    # centroids = lsa.inverse_transform(kmeans.cluster_centers_)
-    # with open('examine_keys.json', 'wb') as f:
-    #     json.dump(show_keywords(centroids, cntvect.get_feature_names()), f)
-    # X_test = [ noaa[0][u'description'] ]
-    # X_test.extend(noaa[0][u'keyword'])
-    # print len(X_test)
-    # cluster_test = pipeline.predict(X_test)
-    # pp.pprint(cluster_test)
-    # print cluster_test.shape
-    # Predict
-    # Build set
-    # X_hat = { entry[u'title']: }
+    # t0 = time.time()
+    # print 'Time started: %s'%t0
+    # clust = KMeans(n_clusters=prop_clust, max_iter=100)
+    # X_clust = clust.fit_transform(X_norm)
+    # print 'Time elapsed: %s'%(time.time()-t0)
+    # centroids = dim.inverse_transform(clust.cluster_centers_)
+    # with open('topic_lsa_kmeans.json', 'wb') as f:
+    #     json.dump(show_keywords(centroids, vect.get_feature_names(), prop_words), f)
+    # print '#####################################################################################################################'
+    # print 'Topic Generation: Latent Dirichlet Allocation'
+    # print '#####################################################################################################################'
+    # t0 = time.time()
+    # print 'Time started: %s'%t0
+    # lda = LatentDirichletAllocation(n_topics=prop_clust, max_iter=5).fit(X_vect)
+    # print 'LDA Time elapsed: %s'%(time.time()-t0)
+    # with open('topic_lda.json', 'wb') as f:
+    #     json.dump(save_top_words(lda, vect.get_feature_names(), prop_words), f)
+    print '#####################################################################################################################'
+    print 'Topic Generation: Dirichlet Process Gaussian Mixture Model'
+    print '#####################################################################################################################'
+    t0 = time.time()
+    print 'Time started: %s'%t0
+    dpgmm = DPGMM(n_components=prop_clust, alpha=100).fit(X_norm)
+    pp.pprint(dpgmm.means_)
+    # pp.pprint(dpgmm.predict(X_norm))
+    print 'DPGMM Time elapsed: %s'%(time.time()-t0)
+    # with open('topic_dpgmm.json', 'wb') as f:
+    #     json.dump(save_top_words(lda, vect.get_feature_names(), prop_words), f)
+
 
 
 
